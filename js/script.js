a10148c8579d5864c4231e07c4b77d0ae3d476b6
LMortinho <lucaslemospm@hotmail.com>
2023-08-02 00:04:16 -0300

    Shiny button added

--- js/script.js
+++ js/script.js
@@ -1,13 +1,16 @@
 const pokemonName = document.querySelector('.pokemon_name');
 const pokemonId = document.querySelector('.pokemon_id');
 const pokemonImage = document.querySelector('.pokemon_image');
+const pokemonShiny = document.querySelector('.pokemon_image');
 
 const form = document.querySelector('.form');
 const input = document.querySelector('.input_search');
 const buttonPrev = document.querySelector('.btn-prev');
 const buttonNext = document.querySelector('.btn-next');
+const buttonShiny = document.querySelector('.btn-shiny');
 
 let searchPokemon = 132;
+var shiny = false;
 
 const fetchPokemon = async (pokemon) => {
     const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
@@ -55,6 +58,20 @@ buttonPrev.addEventListener('click', () => {
 buttonNext.addEventListener('click', () => {
     searchPokemon += 1;
     renderPokemon(searchPokemon);
+});
+
+buttonShiny.addEventListener('click', async () => {
+    if (shiny == true) {
+        const data = await fetchPokemon(searchPokemon);
+
+        pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+        shiny = false;
+    } else {
+        const data = await fetchPokemon(searchPokemon);
+
+        pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_shiny'];
+        shiny = true;
+    }  
 });
 
 
